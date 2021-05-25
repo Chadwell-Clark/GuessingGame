039e9500969ea1d6f7f4ea561be2b730c592d966
Chadwell Clark <wcclark23@comcast.net>
2021-05-25 11:06:27 -0500

    Finished Phase 3 and Phase 4

--- Program.cs
+++ Program.cs
@@ -1,21 +1,49 @@
 ﻿using System;
 
-Console.WriteLine("Can you Guess the Secret Number??? ");
-Console.Write("");
-string response = Console.ReadLine();
-// Console.WriteLine(response);
-string secretNumber = "42";
 
-Compare();
+string response;
+string secretNumber = "42";
+int counter = 0;
+
+
+GuessingGame();
+
+
+
 
 void Compare()
 {
     if (response == secretNumber)
     {
         Console.WriteLine("You guessed correctly!");
+        Environment.Exit(0);
     }
     else
     {
-        Console.WriteLine("You failed!");
+        Console.WriteLine($"Incorrect Guess!({counter})");
     }
 }
+
+void AskQuestion()
+{
+    Console.WriteLine("Can you Guess the Secret Number??? ");
+    Console.Write("");
+    response = Console.ReadLine();
+}
+
+void Loop()
+{
+    for (int i = 1; i <= 4; i++)
+    {
+        counter++;
+        AskQuestion();
+        Compare();
+    }
+
+}
+
+void GuessingGame()
+{
+    Loop();
+}
+
