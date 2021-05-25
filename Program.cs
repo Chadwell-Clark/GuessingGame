77d0ff3185aa3889561d3db84dcae4f2bffa7865
Chadwell Clark <wcclark23@comcast.net>
2021-05-25 10:20:56 -0500

    Phase 1 completed

--- Program.cs
+++ Program.cs
@@ -1,12 +1,6 @@
 ﻿using System;
 
-namespace GuessingGame
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+Console.WriteLine("Can you Guess the Secret Number??? ");
+Console.Write("");
+string response = Console.ReadLine();
+Console.WriteLine(response);
