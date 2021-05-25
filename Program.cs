fc30a9b0a53319249d6813e60ff99ad838820c80
Chadwell Clark <wcclark23@comcast.net>
2021-05-25 10:30:39 -0500

    Phase 2 Completed

--- Program.cs
+++ Program.cs
@@ -3,4 +3,19 @@
 Console.WriteLine("Can you Guess the Secret Number??? ");
 Console.Write("");
 string response = Console.ReadLine();
-Console.WriteLine(response);
+// Console.WriteLine(response);
+string secretNumber = "42";
+
+Compare();
+
+void Compare()
+{
+    if (response == secretNumber)
+    {
+        Console.WriteLine("You guessed correctly!");
+    }
+    else
+    {
+        Console.WriteLine("You failed!");
+    }
+}
